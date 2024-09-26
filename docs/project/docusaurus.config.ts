3c2fce825f496ad3c417d5260aed5a98c55140d2
hirohata <hiroahtatro@gmail.com>
2024-09-25 18:18:12 -0700

    fix: base URL

--- docs/project/docusaurus.config.ts
+++ docs/project/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
 	url: "https://github.com", // TODO: update here after deployed document
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/ExogenesisEnsemble_Part3_Redemption/project",
+	baseUrl: "/ExogenesisEnsemble-Part3-Redemption/project",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
