43976adb5d6bbe0c3b337dea48c674dcafa127a9
hirohata <43802663+hitohata@users.noreply.github.com>
2024-10-01 22:37:59 -0700

    feat: added this error (#48)
    * feat: added this error
    
    * fix: error messages
    
    * fix: added allow macro

---
+++ app/src-tauri/src/errors/mod.rs
@@ -0,0 +1,12 @@
+use thiserror::Error;
+
+/// The errors
+#[derive(Error, Debug, PartialEq)]
+pub enum ExogenesisError {
+    #[allow(dead_code)] // TODO: must be removed
+    #[error("Reading error: {0}")]
+    FileReadError(String),
+    #[allow(dead_code)] // TODO: must be removed
+    #[error("Writing error: {0}")]
+    FileWriteError(String),
+}
