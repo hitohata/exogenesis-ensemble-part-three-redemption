cef7fae0424b1e0781d330d0af2b2a14070f46e8
hitohata <hirohatatro@gmail.com>
2024-10-01 22:35:22 -0700

    fix: added allow macro

--- app/src-tauri/src/errors/mod.rs
+++ app/src-tauri/src/errors/mod.rs
@@ -1,10 +1,12 @@
 use thiserror::Error;
 
 /// The errors
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum ExogenesisError {
+    #[allow(dead_code)] // TODO: must be removed
     #[error("Reading error: {0}")]
     FileReadError(String),
+    #[allow(dead_code)] // TODO: must be removed
     #[error("Writing error: {0}")]
     FileWriteError(String),
 }
