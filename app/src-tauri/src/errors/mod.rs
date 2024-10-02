05dc5a56ebd27f4e97a8d4921b85185fcba3be93
hitohata <hirohatatro@gmail.com>
2024-10-01 22:02:50 -0700

    fix: error messages

--- app/src-tauri/src/errors/mod.rs
+++ app/src-tauri/src/errors/mod.rs
@@ -3,8 +3,8 @@ use thiserror::Error;
 /// The errors
 #[derive(Error, Debug)]
 pub enum ExogenesisError {
-    #[error("Reading error: {:?}")]
+    #[error("Reading error: {0}")]
     FileReadError(String),
-    #[error("Writing error: {:?}")]
+    #[error("Writing error: {0}")]
     FileWriteError(String),
 }
