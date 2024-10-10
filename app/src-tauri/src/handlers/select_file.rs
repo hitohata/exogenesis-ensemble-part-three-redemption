0a91703a97aefdbf2a9306468d7727589a936db2
hirohata <43802663+hitohata@users.noreply.github.com>
2024-10-09 17:52:55 -0700

    feat: added a function (#60)

--- app/src-tauri/src/handlers/select_file.rs
+++ app/src-tauri/src/handlers/select_file.rs
@@ -1,5 +1,12 @@
+use crate::local_file::assign_file;
+
 /// select file by user. The path is passed to this function.
 #[tauri::command]
 pub fn select_file(path: &str) -> String {
-    return String::from(path);
+    let file_name = match assign_file(path) {
+        Ok(file) => file,
+        Err(error) => return error.to_string(),
+    };
+
+    file_name
 }
