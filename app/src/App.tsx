5757642e5f063d345fea5427e11ec3c778a8daa1
hitohata <hirohatatro@gmail.com>
2024-10-12 21:41:08 -0700

    feat: added an invoke function in the front

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,7 @@ import "./index.css";
 import "./App.css";
 import { ModeContext } from "./src/context/ModeContext.tsx";
 import { Layout } from "./src/layout/Layout.tsx";
+import {UploadPage} from "./src/pages/upload/uploadPage.tsx";
 
 function App() {
 	const { mode } = useContext(ModeContext);
@@ -23,57 +24,60 @@ function App() {
 		console.log(mode);
 	}, [mode]);
 
-	useEffect(() => {
-		listen<TauriEvent.DRAG_DROP>(TauriEvent.DRAG_DROP, async (event) => {
-			console.log(event);
-		});
-		listen(TauriEvent.WINDOW_FOCUS, (event) => {
-			console.log(event.payload);
-		});
-		listen(TauriEvent.DRAG_LEAVE, async (event) => {
-			console.log(event);
-		});
-	}, []);
+	// useEffect(() => {
+	// 	listen<TauriEvent.DRAG_DROP>(TauriEvent.DRAG_DROP, async (event) => {
+	// 		console.log(event);
+	// 	});
+	// 	listen(TauriEvent.WINDOW_FOCUS, (event) => {
+	// 		console.log(event.payload);
+	// 	});
+	// 	listen(TauriEvent.DRAG_LEAVE, async (event) => {
+	// 		console.log(event);
+	// 	});
+	// }, []);
 
 	return (
 		<Layout>
-			<div className="container">
-				<h1>Welcome to Tauri!</h1>
+			{/*<div className="container">*/}
 
-				<div className="row">
-					{ mode === "UPLOAD" && <p>vite</p> }
-					<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-						<img src="/vite.svg" className="logo vite" alt="Vite logo" />
-					</a>
-					{ mode === "BACK_UP" && <p>tauri</p> }
-					<a href="https://tauri.app" target="_blank" rel="noreferrer">
-						<img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-					</a>
-					{ mode === "LOCAL" && <p>React</p> }
-					<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-						<img src={reactLogo} className="logo react" alt="React logo" />
-					</a>
-				</div>
+				{ mode === "UPLOAD" && <UploadPage />}
 
-				<p>Click on the Tauri, Vite, and React logos to learn more.</p>
+				{/*<h1>Welcome to Tauri!</h1>*/}
 
-				<form
-					className="row"
-					onSubmit={(e) => {
-						e.preventDefault();
-						greet();
-					}}
-				>
-					<input
-						id="greet-input"
-						onChange={(e) => setName(e.currentTarget.value)}
-						placeholder="Enter a name..."
-					/>
-					<button type="submit">Greet</button>
-				</form>
+				{/*<div className="row">*/}
+				{/*	{ mode === "UPLOAD" && <p>vite</p> }*/}
+				{/*	<a href="https://vitejs.dev" target="_blank" rel="noreferrer">*/}
+				{/*		<img src="/vite.svg" className="logo vite" alt="Vite logo" />*/}
+				{/*	</a>*/}
+				{/*	{ mode === "BACK_UP" && <p>tauri</p> }*/}
+				{/*	<a href="https://tauri.app" target="_blank" rel="noreferrer">*/}
+				{/*		<img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />*/}
+				{/*	</a>*/}
+				{/*	{ mode === "LOCAL" && <p>React</p> }*/}
+				{/*	<a href="https://reactjs.org" target="_blank" rel="noreferrer">*/}
+				{/*		<img src={reactLogo} className="logo react" alt="React logo" />*/}
+				{/*	</a>*/}
+				{/*</div>*/}
 
-				<p>{greetMsg}</p>
-			</div>
+				{/*<p>Click on the Tauri, Vite, and React logos to learn more.</p>*/}
+
+				{/*<form*/}
+				{/*	className="row"*/}
+				{/*	onSubmit={(e) => {*/}
+				{/*		e.preventDefault();*/}
+				{/*		greet();*/}
+				{/*	}}*/}
+				{/*>*/}
+				{/*	<input*/}
+				{/*		id="greet-input"*/}
+				{/*		onChange={(e) => setName(e.currentTarget.value)}*/}
+				{/*		placeholder="Enter a name..."*/}
+				{/*	/>*/}
+				{/*	<button type="submit">Greet</button>*/}
+				{/*</form>*/}
+
+				{/*<p>{greetMsg}</p>*/}
+			{/*</div>*/}
 		</Layout>
 	);
 }
