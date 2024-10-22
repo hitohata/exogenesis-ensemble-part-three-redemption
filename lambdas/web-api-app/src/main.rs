d04accc3779d2182c9a35375def46d5dd5d28155
hirohata <43802663+hitohata@users.noreply.github.com>
2024-10-22 10:06:14 -0700

    Feature/67 set up the web api function (#70)
    * feat: added a lock file
    
    * feat: added the Axum

--- lambdas/web-api-app/src/main.rs
+++ lambdas/web-api-app/src/main.rs
@@ -1,30 +1,20 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use axum::{Json, Router};
+use axum::routing::get;
+use lambda_http::{run, tracing, Error};
+use serde_json::{json, Value};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+fn greet() -> Json<Value> {
+    return Json(json!({"body": "hello world"}))
 }
 
+/// This project uses the [Axum](https://docs.rs/axum/latest/axum/).
+/// The way of adoption of Axum refers to [this repo](https://github.com/awslabs/aws-lambda-rust-runtime/blob/main/examples/http-axum/src/main.rs).
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
 
-    run(service_fn(function_handler)).await
+    let app = Router::new()
+        .route("/", get(greet));
+
+    run(app)
 }
