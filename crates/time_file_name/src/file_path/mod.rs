d9c361b4cc6cf8ac329cac116c72db275d58c016
hitohata <hirohatatro@gmail.com>
2024-11-14 12:30:22 -0800

    feat: added a path parser

--- crates/time_file_name/src/file_path/mod.rs
+++ crates/time_file_name/src/file_path/mod.rs
@@ -1,8 +1,6 @@
 //! This module is for converting from the datetime to the file path
 
-
-use chrono::{DateTime, Datelike, TimeZone, Timelike, Utc};
-use chrono::offset::LocalResult;
+use chrono::{DateTime, Datelike, Timelike, Utc};
 
 /// This struct is designed for generating the file path
 /// This main function is `generate_file_path`, but this struct must be created beforehand.
@@ -30,16 +28,24 @@ use chrono::offset::LocalResult;
 pub struct FilePath {}
 
 impl FilePath {
-
-    pub fn new() -> Self { FilePath{} }
+    pub fn new() -> Self {
+        FilePath {}
+    }
 
     /// Generate a file path
     /// Acceptable type is u128, epoch time, and &str, the ISO 8061 string.
     #[allow(private_bounds)]
-    pub fn generate_file_path<DateTimeType>(&self, date_time: DateTimeType, extension: &str) -> Result<String, String> where Self: GenerateFile<DateTimeType> {
+    pub fn generate_file_path<DateTimeType>(
+        &self,
+        date_time: DateTimeType,
+        extension: &str,
+    ) -> Result<String, String>
+    where
+        Self: GenerateFile<DateTimeType>,
+    {
         self.generate_file_path_from_datetime(date_time, extension)
     }
-    
+
     /// create the file path from the date time.
     /// It is /yyyy/MM/dd/yyyy-MM-dd-hh-mm-ss.{extension}
     fn convert_file_name(
@@ -47,7 +53,6 @@ impl FilePath {
         datetime: DateTime<Utc>,
         extension: &str,
     ) -> Result<String, String> {
-
         let without_dot_extension = match extension.starts_with(".") {
             true => extension.split_at(1).1,
             false => extension,
@@ -67,15 +72,15 @@ impl FilePath {
             without_dot_extension,
         ))
     }
-    
+
     /// Check if the extension is not empty
     fn check_extension(&self, extension: &str) -> Result<(), String> {
         match extension.len() < 1 {
             true => Err("Invalid extension".to_string()),
-            false => Ok(())
+            false => Ok(()),
         }
     }
-    
+
     /// take an epoch time as an argument, then returns the datetime struct
     fn epoch_to_datetime(&self, epoch_time: u128) -> Result<DateTime<Utc>, String> {
         match DateTime::from_timestamp_millis(epoch_time as i64) {
@@ -88,19 +93,30 @@ impl FilePath {
     fn iso_date_time_to_datetime(&self, date_time: &str) -> Result<DateTime<Utc>, String> {
         match date_time.parse::<DateTime<Utc>>() {
             Ok(time) => Ok(time),
-            Err(_) => Err(format!("cannot convert the provided epoch time to datetime: {}", date_time)),
+            Err(_) => Err(format!(
+                "cannot convert the provided epoch time to datetime: {}",
+                date_time
+            )),
         }
     }
 }
 
 trait GenerateFile<DateTimeType> {
     /// A path is created by the date time.
-    fn generate_file_path_from_datetime(&self, date_time: DateTimeType, extension: &str) -> Result<String, String>;
+    fn generate_file_path_from_datetime(
+        &self,
+        date_time: DateTimeType,
+        extension: &str,
+    ) -> Result<String, String>;
 }
 
 impl GenerateFile<u128> for FilePath {
     /// The date time must be the epoch time.
-    fn generate_file_path_from_datetime(&self, date_time: u128, extension: &str) -> Result<String, String> {
+    fn generate_file_path_from_datetime(
+        &self,
+        date_time: u128,
+        extension: &str,
+    ) -> Result<String, String> {
         self.check_extension(extension)?;
         let date_time_chrono = self.epoch_to_datetime(date_time)?;
         self.convert_file_name(date_time_chrono, extension)
@@ -109,62 +125,14 @@ impl GenerateFile<u128> for FilePath {
 
 impl GenerateFile<&str> for FilePath {
     /// date time must be a ISO 8061 date time string
-    fn  generate_file_path_from_datetime(&self, date_time: &str, extension: &str) -> Result<String, String> {
+    fn generate_file_path_from_datetime(
+        &self,
+        date_time: &str,
+        extension: &str,
+    ) -> Result<String, String> {
         self.check_extension(extension)?;
         let date_time_chrono = self.iso_date_time_to_datetime(date_time)?;
         self.convert_file_name(date_time_chrono, extension)
-    }
-}
-
-///  parse the file path
-#[allow(dead_code)] // TODO: must be fixed 
-fn from_file_name_to_date_time(path: &str) -> Result<DateTime<Utc>, String> {
-
-    let file_path = match &path[..1] == "/" {
-        true => path[1..].to_owned(),
-        false => path.to_owned(),
-    };
-
-    println!("file path: {:?}", file_path);
-
-    let vec_path = file_path.split("/").collect::<Vec<&str>>();
-
-    // it will be four elements, year, month, day, file name
-    if vec_path.len() != 4 {
-       return Err("invalid file path is provided".to_string());
-    };
-
-    let vec_file_name = vec_path[3].split("-").collect::<Vec<&str>>();
-    // it will be four elements, year, month, day, hour, minute, sec with extension
-    if vec_file_name.len() != 6 {
-        return Err("invalid file name is provided".to_string());
-    }
-
-    let sec_str = vec_file_name[5].split(".").collect::<Vec<&str>>()[0];
-
-    let Ok(year) = vec_path[0].parse::<i32>() else {
-        return Err(format!("invalid year in the file name: {}", vec_path[0]));
-    };
-    let Ok(month) = vec_path[1].parse::<u32>() else {
-        return Err(format!("invalid month in the file name: {}", vec_path[1]));
-    };
-    let Ok(day) = vec_path[2].parse::<u32>() else {
-        return Err(format!("invalid day in the file name: {}", vec_path[2]));
-    };
-    let Ok(hour) = vec_file_name[3].parse::<u32>() else {
-        return Err(format!("invalid hour in the file name: {}", vec_file_name[3]));
-    };
-    let Ok(minute) = vec_file_name[4].parse::<u32>() else {
-        return Err(format!("invalid minute in the file name: {}", vec_file_name[4]));
-    };
-    let Ok(sec) = sec_str.parse::<u32>() else {
-        return Err(format!("invalid sec in the file name: {}", sec_str));
-    };
-
-    match Utc.with_ymd_and_hms(year, month, day, hour, minute, sec) {
-        LocalResult::Single(datetime) => Ok(datetime),
-        LocalResult::Ambiguous(_, _) => Err(format!("Ambiguous path: {}", path)),
-        _ => Err("Invalid path is provided, so cannot convert it.".to_string())
     }
 }
 
@@ -176,14 +144,16 @@ mod test_generate_file_path {
     #[test]
     fn test_generate_file_path() {
         // Assert
-        const YEAR: i32  = 1984;
-        const MONTH: u32  = 4;
-        const DAY: u32  = 4;
-        const HOUR: u32  = 12;
-        const MIN: u32  = 42;
-        const SEC: u32  = 42;
+        const YEAR: i32 = 1984;
+        const MONTH: u32 = 4;
+        const DAY: u32 = 4;
+        const HOUR: u32 = 12;
+        const MIN: u32 = 42;
+        const SEC: u32 = 42;
 
-        let date_time = Utc.with_ymd_and_hms(YEAR, MONTH, DAY, HOUR, MIN, SEC).unwrap();
+        let date_time = Utc
+            .with_ymd_and_hms(YEAR, MONTH, DAY, HOUR, MIN, SEC)
+            .unwrap();
         let extension = ".VIDEO";
 
         // Act
@@ -230,7 +200,9 @@ mod test_iso_date_time_to_datetime {
         let date_time = "1984-04-04T12:34:50Z";
 
         // Acr
-        let result = FilePath::new().iso_date_time_to_datetime(date_time).unwrap();
+        let result = FilePath::new()
+            .iso_date_time_to_datetime(date_time)
+            .unwrap();
 
         // Assert
         assert_eq!(result.year(), 1984);
@@ -239,81 +211,5 @@ mod test_iso_date_time_to_datetime {
         assert_eq!(result.hour(), 12);
         assert_eq!(result.minute(), 34);
         assert_eq!(result.second(), 50);
-    }
-}
-
-#[cfg(test)]
-mod test_from_file_name_to_date_time {
-    use super::*;
-
-    #[test]
-    fn test_normal_path() {
-        // Assert
-        let path = "/1984/04/04/1984-4-4-12-34-56.video";
-
-        // Act
-        let result = from_file_name_to_date_time(path).unwrap();
-
-        // Assert
-        assert_eq!(result.year(), 1984);
-        assert_eq!(result.month(), 4);
-        assert_eq!(result.day(), 4);
-        assert_eq!(result.hour(), 12);
-        assert_eq!(result.minute(), 34);
-        assert_eq!(result.second(), 56);
-    }
-
-    #[test]
-    fn test_normal_path_without_slash() {
-        // Assert
-        let path = "1984/04/04/1984-4-4-12-34-56.video";
-
-        // Act
-        let result = from_file_name_to_date_time(path).unwrap();
-
-        // Assert
-        assert_eq!(result.year(), 1984);
-        assert_eq!(result.month(), 4);
-        assert_eq!(result.day(), 4);
-        assert_eq!(result.hour(), 12);
-        assert_eq!(result.minute(), 34);
-        assert_eq!(result.second(), 56);
-    }
-
-    #[test]
-    fn test_invalid_year() {
-        // Assert
-        let path = "/hoge/04/04/1984-4-4-12-34-56.video";
-
-        // Act
-        let result = from_file_name_to_date_time(path);
-
-        // Assert
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), "invalid year in the file name: hoge");
-    }
-
-    #[test]
-    fn test_invalid_file_length() {
-        // Assert
-        let path = "/1984/04/1984-4-4-12-34-56.video";
-
-        // Act
-        let result = from_file_name_to_date_time(path);
-
-        // Assert
-        assert!(result.is_err());
-    }
-    
-    #[test]
-    fn test_invalid_file_name() {
-        // Assert
-        let path = "/1984/04/04/1984-4-4-34-56.video";
-
-        // Act
-        let result = from_file_name_to_date_time(path);
-
-        // Assert
-        assert!(result.is_err());
     }
 }
--- crates/time_file_name/src/lib.rs
+++ crates/time_file_name/src/lib.rs
@@ -2,4 +2,43 @@
 //! This crate handles file name and datetime
 //! The local file name will be /yyyy/MM/dd/yyyy-MM-dd-hh-mm-ss.{extension}
 
+pub mod file_datetime;
 pub mod file_path;
+
+#[cfg(test)]
+/// This test is of checking if the date time and path are re-convert
+mod test {
+    use crate::file_datetime::PathDateTime;
+    use crate::file_path::FilePath;
+    use chrono::{TimeZone, Utc};
+
+    #[test]
+    fn time_to_time() {
+        // Arrange
+        let date_time = Utc.with_ymd_and_hms(1984, 4, 4, 12, 34, 50).unwrap();
+
+        // Act
+        let path = FilePath::new()
+            .generate_file_path(date_time.to_rfc3339().as_str(), "video")
+            .unwrap();
+        let result = PathDateTime::parse(path.as_str()).unwrap();
+
+        // Assert
+        assert_eq!(result.unix_time, date_time.timestamp_millis());
+    }
+
+    #[test]
+    fn string_to_string() {
+        // Arrange
+        let date_time = "1984-04-04T12:34:50+00:00";
+
+        // Act
+        let path = FilePath::new()
+            .generate_file_path(date_time, "video")
+            .unwrap();
+        let result = PathDateTime::parse(path.as_str()).unwrap();
+
+        // Assert
+        assert_eq!(result.iso_string, date_time);
+    }
+}
